8372ca063bfec17e9ac4bddfc6e643d6e3a6c4c0
ChristosCM <christoumavropoulos.christos@gmail.com>
2019-02-13 00:02:53 +0000

    encoder works, might need some improvements for bits, decoder needs finalising

--- encoder.py
+++ encoder.py
@@ -1,13 +1,41 @@
+from bitarray import bitarray
+import sys
 def getFile():
     fileName = input("Please provide the name of the file you wish to compress: ")
     try:
-        file = open(fileName, "r")
+        file = open(fileName, "rb")
     except IOError:
         print("File was not found")
     data = file.read()
     file.close()
     return data
 
+def match(data,pos,L,W):
+    length = -1
+    distance = -1
+    end = min(pos + L, len(data) + 1)
+    for j in range (pos+1, end):
+        start = max(0, pos - W)
+        substring = data[pos:j]
+        #this is another implementation that uses rfind, doesnt work as of now but can use it to make the program faster
+        # string = data.rfind(substring,start,end)
+        # if (string!=-1 and len(substring) >length):
+        #     distance = string
+        #     length = len(substring)
+        for i in range(start, pos):	
+            #this only looks at the l sized windows as a bigger one would not make a difference as it wouldn't match and check them
+            string = data[i:i+j-pos]
+            if (string == substring and len(substring) >=length):
+                distance = pos -i
+                length = len(substring)
+    if (distance > 0 and length >0):
+        return (distance, length)
+    return None
+def write(bits):
+    file = open("compressed.bin","wb")
+    file.write(bits.tobytes())
+    file.close()
+    return None
 #data for the lz encoder needs to be a string of characters for it to work
 def lzEncoder(data,W,L):
     if L>W:
@@ -16,53 +44,34 @@ def lzEncoder(data,W,L):
     
     #encoded is an empty array of arrays. each sub-array is a triple based on LZ77
     encoded=[]
-    #done is boolean that signifies that the whole document has been encoded
-    done = False 
+
+    output = bitarray(endian='big')
     # pos is the coding position 
     pos = 0;
     #first element is always going to appear as that so we append it and put pos in the next position
     encoded.append((0,0,data[0]))
     pos = 1;
-
-    while done==False:
-        slidingW = []
-        for l in range (pos+1,L):
-            #check if the l is over the end of the document and close
-            if l>len(data):
-                done = True;
-                break
-            #check if the sliding window is over the start of the document and adjust it 
-            startWindow = max(0, pos - W)
+    while pos<len(data):
+        matching = match(data,pos,L,W)
+        #this if checks if a match was found or not
+        if matching:
+            (dis, length) = matching
+            encoded.append((dis,length,data[pos+length]))
+            output.append(True)
+            output.frombytes(chr(dis>>4).encode())
+            output.frombytes(chr(((dis & 0xf) << 4) | length).encode())
+            pos +=length+1
+                
+                
+        #since a match was not found, the input will just be by itself with the next character
+        else:
+            encoded.append((0,0,data[pos]))
+            output.append(False)
+            output.frombytes(chr(data[pos]).encode())
             
-            for i in range(pos-1,pos-newW,-1):
-                slidingW.append(data[pos-i])
-                print (slidingW)
             pos +=1
-        break
-        
 
-def match(data,pos,L,W):
-    length = -1
-    distance = -1
-    end = min(pos + L, len(data) + 1)
-    for j in range(pos + 1, end):
-		
-        start = max(0, pos - W)
-		substring = data[pos:j]
-
-		for i in range(start, pos):	
-
-            #this only looks at the l sized windows as a bigger one would not make a difference as it wouldn't match and check them
-			string = data[i:i+j-pos] 
-
-			if string == substring and len(substring) > length:
-				distance = pos - i 
-				length = len(substring)		
-
-		if distance > 0 and length > 0:
-			return (distance, length)
-		return None
+    write(output)     
 
 
-
-lzEncoder(getFile(),6,6)
+lzEncoder(getFile(),50,6)
